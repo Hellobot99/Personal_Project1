942ecf60552835730a55b003382515d3daffb6bf
Hellobot99 <ktj4060@naver.com>
2025-03-15 19:00:13 +0900

    initial push

---
+++ personal_project1/src/test/java/com/ktj4060/personal_project1/PersonalProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktj4060.personal_project1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
